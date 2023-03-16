9605123c3543eae102be9093e89be978f3ef61c6
abhishekvadve <76932751+abhishekvadve@users.noreply.github.com>
2023-03-17 00:18:38 +0530

    commit index

--- predictions/views.py
+++ predictions/views.py
@@ -89,8 +89,8 @@ def calculate_predictions(request):
                 with open('realtime_data_plot.png', 'rb') as f:
                     context['realtime_data_plot'] = f.read()
                     context['form'] = form
-        return render(request, 'predictions.html', context)
+        return render(request, 'index.html', context)
     else:
         form = PredictionsForm()
         context = {'form': form}
-        return render(request, 'predictions.html', context)
+        return render(request, 'index.html', context)
